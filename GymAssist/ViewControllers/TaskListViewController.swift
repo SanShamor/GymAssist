13093bdfd6ca32ab1bcbd6e125b213bd4e0690e3
SanShamor <sanshamor@mail.ru>
2021-09-15 00:17:48 +0300

    - AlertController - createTestData

--- GymAssist/ViewControllers/TaskListViewController.swift
+++ GymAssist/ViewControllers/TaskListViewController.swift
@@ -5,24 +5,77 @@
 //  Created by Александр on 14.09.2021.
 //
 
-import UIKit
+import RealmSwift
 
 class TaskListViewController: UITableViewController {
+
+    private var taskLists: Results<TaskList>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        taskLists = StorageManager.shared.realm.objects(TaskList.self)
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        taskLists.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskListCell", for: indexPath)
+        let taskList = taskLists[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = taskList.name
+        content.secondaryText = "\(taskList.tasks.count)"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        guard let tasksVC = segue.destination as? TasksViewController else { return }
+        let taskList = taskLists[indexPath.row]
+    }
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        showAlert()
+    }
+    
+    @IBAction func sortingList(_ sender: UISegmentedControl) {
+    }
+    
+    private func createTestData() {
+        let shoppingList = TaskList()
+        shoppingList.name = "Силовая треня"
+        
+        let moviesList = TaskList(value: ["Кардио треня", Date(), [["Best film ever"], ["The best of the best", "Must have", Date(), true]]])
+        
+        let milk = Task()
+        milk.name = "Гантели"
+        milk.note = "8Kg"
+        
+        let bread = Task(value: ["Подтягивания", "", Date(), true])
+        let apples = Task(value: ["name": "Жим штанги", "note": "10Kg"])
+        
+        shoppingList.tasks.append(milk)
+        shoppingList.tasks.insert(contentsOf: [bread, apples], at: 1)
+        
+        DispatchQueue.main.async {
+            StorageManager.shared.save(taskLists: [shoppingList, moviesList])
+        }
+    }
+}
+
+extension TaskListViewController {
+    
+    private func showAlert() {
+        let alert = AlertController(title: "Новая тренировка", message: "Пожалуйста, введите данные", preferredStyle: .alert)
+        
+        alert.action { newValue in
+            
+        }
+        present(alert, animated: true)
+    }
     
 }
