8d247a696412856de65268834ac9b92b7e066bca
SanShamor <sanshamor@mail.ru>
2021-09-19 21:29:49 +0300

    Add new VC with UITimer

---
+++ GymAssist/ViewControllers/DetailTaskViewController.swift
@@ -0,0 +1,79 @@
+//
+//  DetailTaskViewController.swift
+//  GymAssist
+//
+//  Created by Александр on 19.09.2021.
+//
+
+import UIKit
+
+class DetailTaskViewController: UIViewController {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var timer: Timer = Timer()
+    var count: Int = 0
+    var timerCounting: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startStopButton.setTitleColor(UIColor.green, for: .normal)
+    }
+    
+    @IBAction func startStopTapped(_ sender: Any) {
+        if timerCounting {
+            timerCounting = false
+            timer.invalidate()
+            startStopButton.setTitle("START", for: .normal )
+            startStopButton.setTitleColor(UIColor.green, for: .normal)
+
+        } else {
+            timerCounting = true
+            startStopButton.setTitle("STOP", for: .normal)
+            startStopButton.setTitleColor(UIColor.red, for: .normal)
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func resetTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Reset timer?", message: "Are you sure?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (_) in
+            self.count = 0
+            self.timer.invalidate()
+            self.timerLabel.text = self.makeTimeString(hours: 0, minutes: 0, seconds: 0)
+            self.startStopButton.setTitle("START", for: .normal )
+            self.startStopButton.setTitleColor(UIColor.green, for: .normal)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "CANCEL", style: .default, handler: { (_) in
+            // do nothing
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func timerCounter() -> Void {
+        count = count + 1
+        let time = secondsToHoursMinutesSeconds(seconds: count)
+        let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
+        timerLabel.text = timeString
+    }
+    
+    func secondsToHoursMinutesSeconds(seconds: Int) -> (Int, Int, Int) {
+        return ((seconds / 3600), ((seconds % 3600) / 60), ((seconds % 3600) % 60))
+    }
+        
+    func makeTimeString(hours: Int, minutes: Int, seconds: Int) -> String {
+        var timeString = ""
+        
+        timeString += String(format: "%02d", hours)
+        timeString += " : "
+        timeString += String(format: "%02d", minutes)
+        timeString += " : "
+        timeString += String(format: "%02d", seconds)
+        return timeString
+    }
+}
