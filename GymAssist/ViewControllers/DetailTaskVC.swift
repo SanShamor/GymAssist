076216aab82d18176f481b96b927a7ddcec99bd6
SanShamor <sanshamor@mail.ru>
2021-12-20 01:38:46 +0300

    *Timer working in background now: - new brunch - used UserDefaults and typeDate() properties

--- GymAssist/ViewControllers/DetailTaskVC.swift
+++ GymAssist/ViewControllers/DetailTaskVC.swift
@@ -26,42 +26,73 @@ class DetailTaskVC: UIViewController {
     @IBOutlet weak var previousExerciseButton: UIButton!
     
     // MARK: - Properties
-    var usersExercises: TaskList!
-    private var lapsResults: [String] = []
+    var currentTaskList: TaskList!
     private var exerciseNames: [String] = []
     private var exerciseDescription: [String] = []
+    private var lapsResults: [String] = []
     private var exercisePositionInList = 0
     private var roundsCount = 1
     private var finishedRounds = 0
     
-    private var timerTotal: Timer = Timer()
-    private var countTimerTotal = 0
-    private var countTimerForLap = 0
-    private var timerTotalCountingIsWorking = false
+    private var startTime: Date?
+    private var stopTime: Date?
+    private var lapStartTime = Date()
+    private var lapStopTime = Date()
+    
+    private let userDefaults = UserDefaults.standard
+    private let START_TIME_KEY = "startTime"
+    private let STOP_TIME_KEY = "stopTime"
+    private let COUNTING_KEY = "countingKey"
+    private var timerCounting: Bool = false
+    private var scheduledTimer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        exerciseNames = getExercises(taskList: usersExercises)
-        exerciseDescription = getDescriptions(taskList: usersExercises)
-        setStartValue()
-                
-        startStopTimerButton.setTitleColor(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1), for: .normal)
-        startStopTimerButton.backgroundColor = #colorLiteral(red: 0.1900779605, green: 0.5983788371, blue: 0.4619213343, alpha: 1)
-        resetTimerButton.backgroundColor = #colorLiteral(red: 0.9988579154, green: 0.1766675115, blue: 0.1742589176, alpha: 1)
+        
+        startTime = userDefaults.object(forKey: START_TIME_KEY) as? Date
+        stopTime = userDefaults.object(forKey: STOP_TIME_KEY) as? Date
+        timerCounting = userDefaults.bool(forKey: COUNTING_KEY)
+        
+        setStartValues()
+        setDefaultUI()
+        
+        if timerCounting {
+            startTimer()
+        } else {
+            //если таймер был уже запущен, но остановлен
+            stopTimer()
+            guard let start = startTime else { return }
+            guard let stop = stopTime else { return }
+            let time = calcRestartTime(start: start, stop: stop)
+            let diff = Date().timeIntervalSince(time)
+            setTimerLabel(Int(diff))
+        }
+        
     }
     
     // MARK: - IBActions
     @IBAction func startStopButtonTapped(_ sender: Any) {
         
-        if timerTotalCountingIsWorking {
-            stoppingTimer()
-            setInterfaceWhenTimerStop()
-            timerTotalCountingIsWorking = false
+        if timerCounting {
+            setStopTime(date: Date())
+            lapStopTime = Date()
+            stopTimer()
+            setUIWhenTimerStop()
         } else {
-            timerTotalCountingIsWorking = true
-            setInterfaceWhenTimerRun()
-            timerTotal = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true
-            )
+            //Продолжить таймер после паузы
+            if let stop = stopTime {
+                let restartTime = calcRestartTime(start: startTime!, stop: stop)
+                setStopTime(date: nil)
+                setStartTime(date: restartTime)
+                let restartLapTime = calcRestartTime(start: lapStartTime, stop: lapStopTime)
+                lapStartTime = restartLapTime
+            } else {
+                //Запустить таймер с нуля
+                setStartTime(date: Date())
+                lapStartTime = Date()
+            }
+            
+            startTimer()
         }
     }
     
@@ -69,40 +100,49 @@ class DetailTaskVC: UIViewController {
         let time = getTimerResultValue()
         saveHighscore(withTime: time)
         saveResultButton.isHidden = true
+        
+        let alert = UIAlertController(title: "Saved successfully", message: "You can find it at results-screen", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (_) in
+            // nothing
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
     }
     
     @IBAction func resetButtonTapped(_ sender: Any) {
-        let alert = UIAlertController(title: "Reset timer?", message: "Are you sure?", preferredStyle: .alert)
         
+        let alert = UIAlertController(title: "Reset timer?", message: "Are you sure?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (_) in
-            self.timerTotal.invalidate()
-            self.resetValues()
-            self.countTimerTotal = 0
+            self.setStopTime(date: nil)
+            self.setStartTime(date: nil)
+            self.stopTimer()
+            self.setDefaultUI()
+            self.setStartValues()
+            self.resetTimerButton.isHidden = true
             
             self.timerLabel.textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
             self.startStopTimerButton.backgroundColor = #colorLiteral(red: 0.1900779605, green: 0.5983788371, blue: 0.4619213343, alpha: 1)
             self.startStopTimerButton.setTitle("START", for: .normal )
-            self.timerLabel.text = self.makeTimeString(hours: 0, minutes: 0, seconds: 0)
+            self.timerLabel.text = self.makeTimeString(hour: 0, min: 0, sec: 0)
             self.startStopTimerButton.setTitleColor(
                 #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), for: .normal)
         }))
-        
         alert.addAction(UIAlertAction(title: "CANCEL", style: .default, handler: { (_) in
             // nothing
         }))
-        
         self.present(alert, animated: true, completion: nil)
+        
     }
     
     @IBAction func nextExerciseButtonTapped(_ sender: Any) {
         calculatingRounds()
         if roundComparison() {
-            changeExerciseNext(taskList: usersExercises)
+            changeExerciseNext(taskList: currentTaskList)
         }
     }
     
     @IBAction func previousExerciseButtonTapped(_ sender: Any) {
-        changeExercisePrevious(taskList: usersExercises)
+        changeExercisePrevious(taskList: currentTaskList)
     }
     
     @IBAction func addRoundButtonTapped(_ sender: Any) {
@@ -117,35 +157,98 @@ class DetailTaskVC: UIViewController {
         }
     }
     
-    // MARK: - Timer's Methods
-    @objc func timerCounter() -> Void {
-        countTimerTotal = countTimerTotal + 1
-        countTimerForLap = countTimerForLap + 1
-        let time = secondsToHoursMinutesSeconds(seconds: countTimerTotal)
-        let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
+    // MARK: Methods for Timer
+    @objc func refreshValue() {
+        if let start = startTime {
+            let diff = Date().timeIntervalSince(start)
+            setTimerLabel(Int(diff))
+        } else {
+            stopTimer()
+            setTimerLabel(0)
+        }
+    }
+    
+    func startTimer() {
+        print("ветка работы в фоне 2")
+        scheduledTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(refreshValue), userInfo: nil, repeats: true)
+        setTimerCounting(true)
+        setUIWhenTimerRun()
+    }
+    
+    func stopTimer() {
+        if scheduledTimer != nil {
+            scheduledTimer.invalidate()
+        }
+        setTimerCounting(false)
+    }
+    
+    func setTimerLabel(_ val: Int) {
+        let time = secondsToHoursMinutesSeconds(val)
+        let timeString = makeTimeString(hour: time.0, min: time.1, sec: time.2)
         timerLabel.text = timeString
     }
     
-    private func secondsToHoursMinutesSeconds(seconds: Int) -> (Int, Int, Int) {
-        return ((seconds / 3600), ((seconds % 3600) / 60), ((seconds % 3600) % 60))
+    func secondsToHoursMinutesSeconds(_ ms: Int) -> (Int, Int, Int) {
+        let hour = ms / 3600
+        let min = (ms % 3600) / 60
+        let sec = (ms % 3600) % 60
+        return (hour, min, sec)
     }
     
-    private func makeTimeString(hours: Int, minutes: Int, seconds: Int) -> String {
+    func makeTimeString(hour: Int, min: Int, sec: Int) -> String {
         var timeString = ""
-        
-        timeString += String(format: "%02d", hours)
+        timeString += String(format: "%02d", hour)
         timeString += " : "
-        timeString += String(format: "%02d", minutes)
+        timeString += String(format: "%02d", min)
         timeString += " : "
-        timeString += String(format: "%02d", seconds)
+        timeString += String(format: "%02d", sec)
         return timeString
     }
     
-    private func stoppingTimer() {
-        timerTotal.invalidate()
+    func calcRestartTime(start: Date, stop: Date) -> Date {
+        let diff = start.timeIntervalSince(stop)
+        return Date().addingTimeInterval(diff)
     }
     
-    private func setInterfaceWhenTimerStop() {
+    func setStartTime(date: Date?) {
+        startTime = date
+        userDefaults.set(date, forKey: START_TIME_KEY)
+    }
+    
+    func setStopTime(date: Date?) {
+        stopTime = date
+        userDefaults.set(stopTime, forKey: STOP_TIME_KEY)
+    }
+    
+    func setTimerCounting(_ val: Bool) {
+        timerCounting = val
+        userDefaults.set(timerCounting, forKey: COUNTING_KEY)
+    }
+    
+    private func getTimerResultValue() -> String {
+        guard let start = startTime else { return "Time-Eror" }
+        guard let stop = stopTime else { return "Time-Eror" }
+        let time = calcRestartTime(start: start, stop: stop)
+        let diff = Date().timeIntervalSince(time)
+        let detailTime = secondsToHoursMinutesSeconds(Int(diff))
+        let timeInString = makeTimeString(hour: detailTime.0, min: detailTime.1, sec: detailTime.2)
+        
+        return timeInString
+    }
+    
+    private func saveLapTime() {
+        let currentDate = Date()
+        let time = calcRestartTime(start: lapStartTime, stop: currentDate)
+        let diff = Date().timeIntervalSince(time)
+        let detailTime = secondsToHoursMinutesSeconds(Int(diff))
+        let timeString = makeTimeString(hour: detailTime.0, min: detailTime.1, sec: detailTime.2)
+        let roundResult = "\(finishedRounds)) \(timeString)"
+        lapsResults.append(roundResult)
+        lapStartTime = Date()
+    }
+    
+    // MARK: Methods for UI
+    private func setUIWhenTimerStop() {
         resetTimerButton.isHidden = false
         addRoundButton.isHidden = true
         subRoundButton.isHidden = true
@@ -159,7 +262,7 @@ class DetailTaskVC: UIViewController {
         startStopTimerButton.setTitleColor(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), for: .normal)
     }
     
-    private func setInterfaceWhenTimerRun() {
+    private func setUIWhenTimerRun() {
         addRoundButton.isHidden = true
         subRoundButton.isHidden = true
         resetTimerButton.isHidden = true
@@ -178,32 +281,26 @@ class DetailTaskVC: UIViewController {
         }
     }
     
-    // MARK: - Methods
-    private func getDescriptions(taskList: TaskList) -> [String] {
-        var allDescriptionOfNotes: [String] = []
+    private func setDefaultUI() {
+        saveResultButton.isHidden = true
+        nextExerciseButton.isHidden = true
+        previousExerciseButton.isHidden = true
+        timerLabel.isHidden = false
+        startStopTimerButton.isHidden = false
+        addRoundButton.isHidden = false
+        subRoundButton.isHidden = false
+        roundsCountLabel.isHidden = false
+        userScoreLabel.isHidden = false
         
-        for taskNote in taskList.tasks {
-            allDescriptionOfNotes.append(taskNote.note)
-        }
-        return allDescriptionOfNotes
+        exerciseNameLabel.isHidden = false
+        exerciseDescriptionTextView.isHidden = false
+        resetTimerButton.isHidden = false
+        startStopTimerButton.setTitleColor(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1), for: .normal)
+        startStopTimerButton.backgroundColor = #colorLiteral(red: 0.1900779605, green: 0.5983788371, blue: 0.4619213343, alpha: 1)
+        resetTimerButton.backgroundColor = #colorLiteral(red: 0.9988579154, green: 0.1766675115, blue: 0.1742589176, alpha: 1)
     }
     
-    private func getExercises(taskList: TaskList) -> [String] {
-        var array: [String] = []
-        for task in taskList.tasks {
-            array.append(task.name)
-        }
-        return array
-    }
-    
-    private func calculatingRounds() {
-        roundsCount = Int(String(roundsCountLabel.text ?? "1")) ?? 1
-    }
-    
-    private func roundComparison() -> Bool {
-        finishedRounds < roundsCount
-    }
-    
+    // MARK: Methods
     private func changeExerciseNext(taskList: TaskList) {
         if exercisePositionInList < (taskList.tasks.count - 1) {
             exercisePositionInList += 1
@@ -229,29 +326,51 @@ class DetailTaskVC: UIViewController {
         }
     }
     
-    private func setStartValue() {
+    private func calculatingRounds() {
+        roundsCount = Int(String(roundsCountLabel.text ?? "1")) ?? 1
+    }
+    
+    private func roundComparison() -> Bool {
+        finishedRounds < roundsCount
+    }
+    
+    private func setStartValues() {
+        exerciseNames = getExercises(taskList: currentTaskList)
+        exerciseDescription = getDescriptions(taskList: currentTaskList)
+        exerciseNameLabel.text = (exerciseNames[exercisePositionInList])
+        exerciseDescriptionTextView.text = (exerciseDescription[exercisePositionInList])
+        
         exercisePositionInList = 0
         roundsCount = 1
         lapsResults = []
+        finishedRounds = 0
+        userScoreLabel.text = "Rounds:"
         roundsCountLabel.text = String(roundsCount)
-        exerciseNameLabel.text = (exerciseNames[exercisePositionInList])
-        exerciseDescriptionTextView.text = (exerciseDescription[exercisePositionInList])
     }
     
-    private func resetValues() {
-        setStartValue()
-        finishedRounds = 0
-        userScoreLabel.text = ""
+    private func getExercises(taskList: TaskList) -> [String] {
+        var array: [String] = []
+        for task in taskList.tasks {
+            array.append(task.name)
+        }
+        return array
+    }
+    
+    private func getDescriptions(taskList: TaskList) -> [String] {
+        var allDescriptionOfNotes: [String] = []
         
-        timerLabel.isHidden = false
-        saveResultButton.isHidden = true
-        nextExerciseButton.isHidden = true
-        previousExerciseButton.isHidden = true
-        resetTimerButton.isHidden = true
-        startStopTimerButton.isHidden = false
-        addRoundButton.isHidden = false
-        subRoundButton.isHidden = false
-        roundsCountLabel.isHidden = false
+        for taskNote in taskList.tasks {
+            allDescriptionOfNotes.append(taskNote.note)
+        }
+        return allDescriptionOfNotes
+    }
+    
+    private func getLapResults() -> String {
+        var lapsInfo = ""
+        for lap in lapsResults {
+            lapsInfo += "\n\(lap)"
+        }
+        return lapsInfo
     }
     
     private func finishRound() {
@@ -264,49 +383,27 @@ class DetailTaskVC: UIViewController {
     }
     
     private func finishWorkout() {
-        let totalWorkoutTime = getTimerResultValue()
-        let lapsResult = showTimeResultInLabel()
-        exerciseNameLabel.text = "Тренировка окончена\n\(totalWorkoutTime)"
+        setStopTime(date: lapStartTime)
+        let timeString = getTimerResultValue()
+        exerciseNameLabel.text = "Тренировка окончена\n\(timeString)"
+        let lapsResult = getLapResults()
         exerciseDescriptionTextView.text = "\(lapsResult)"
         saveResultButton.isHidden = false
-        timerTotalCountingIsWorking = false
         startStopTimerButton.isHidden = true
         timerLabel.isHidden = true
         resetTimerButton.isHidden = true
         userScoreLabel.isHidden = true
         
-        stoppingTimer()
-        setInterfaceWhenTimerStop()
-    }
-    
-    private func saveLapTime() {
-        let time = secondsToHoursMinutesSeconds(seconds: countTimerForLap)
-        let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
-        let roundResult = "\(finishedRounds)) \(timeString)"
-        lapsResults.append(roundResult)
-        countTimerForLap = 0
-    }
-    
-    private func getTimerResultValue() -> String {
-        let time = secondsToHoursMinutesSeconds(seconds: countTimerTotal)
-        let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
-        return timeString
-    }
-    
-    private func showTimeResultInLabel() -> String {
-        var lapsInfo = ""
-        for lap in lapsResults {
-            lapsInfo += "\n\(lap)"
-        }
-        return lapsInfo
+        stopTimer()
+        setUIWhenTimerStop()
     }
     
 }
-
+    // MARK: Extension
 extension DetailTaskVC {
     private func saveHighscore(withTime timeResult: String) {
         let result = Highscore()
         result.totalTime = timeResult
-        StorageManager.shared.save(highscore: result, in: usersExercises)
+        StorageManager.shared.save(highscore: result, in: currentTaskList)
     }
 }
